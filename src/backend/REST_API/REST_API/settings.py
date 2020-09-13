0fcb83734c30760cf50bbad627565622fab1bc2a
Otávio Augusto Silva <otavio.silva@dcc.ufmg.br>
2020-09-13 16:37:45 -0300

    Mudou estilo de código

--- src/backend/REST_API/REST_API/settings.py
+++ src/backend/REST_API/REST_API/settings.py
@@ -68,16 +68,16 @@ TEMPLATES = [
 	},
 ]
 
-WSGI_APPLICATION = 'REST_API.wsgi.application'
+WSGI_APPLICATION = "REST_API.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	"default": {
+		"ENGINE": "django.db.backends.sqlite3",
+		"NAME": os.path.join(BASE_DIR, "db.sqlite3"),
 	}
 }
 
@@ -87,16 +87,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
 	{
-		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+		"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
 	},
 	{
-		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+		"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
 	},
 	{
-		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+		"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
 	},
 	{
-		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+		"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
 	},
 ]
 
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -118,4 +118,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
